7f008c79488989e4e204ed6428245817a763f19b
Saba Teklu <saab.teklu@gmail.com>
2021-01-14 16:18:09 -0700

    data seeding configured using images from saved in S3

--- database/seed.js
+++ database/seed.js
@@ -3,24 +3,32 @@ const faker = require('faker');
 
 const Adventures = require('./Adventures.js');
 
+
 function getRandomInt(max) {
-  return Math.random() * Math.floor(max);
+  return (Math.random() * Math.floor(max)).toFixed(2);
 };
 
-const adventureSeed = [
-  {
+function picUrl(num) {
+  let base = `https://tutorial90005123.s3.us-east-2.amazonaws.com/thailandPics/tripadvisor_thailand_${num}.jpg`;
+
+  return base;
+}
+
+let returnAdventure = function (num) {
+  return {
     name: faker.address.streetName(),
-    image: faker.image.city(),
+    image: picUrl(num),
     reviews: faker.random.number(),
     rating: getRandomInt(5),
-    price: faker.commerce.price(),
+    price: `$${faker.commerce.price()}`,
     liked: faker.random.boolean(),
     timesBooked: faker.random.number()
   }
-]
+}
 
-const insertAdventures = function () {
-  Adventures.create(adventureSeed)
+
+for (var i = 0; i < 100; i++) {
+  Adventures.create(returnAdventure(i))
   .then((results)=> {
     console.log(results);
   })
@@ -28,4 +36,4 @@ const insertAdventures = function () {
     console.log(err)
   })
 }
-insertAdventures();
+
