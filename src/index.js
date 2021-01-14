d27d7f20c0aece0228af7f1429a82d4123ea0e4a
Saba Teklu <saab.teklu@gmail.com>
2021-01-13 21:08:28 -0700

    clientside and server side boiler plate set up

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import './styles/style.css'
+
+const App = ()=> {
+  return (
+    <h2>im a maus in a haus </h2>
+  )
+}
+
+reactDOM.render(<App />, document.getElementById('root'));
