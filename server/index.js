6fb1f7a0b7a16835872e040a9d893b6990964ccc
Saba Teklu <saab.teklu@gmail.com>
2021-01-15 09:44:18 -0700

    server routes created and working

--- server/index.js
+++ server/index.js
@@ -1,14 +1,58 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const Adventures = require('../database/index.js');
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '../public/'));
+app.use(express.static(path.join(__dirname, '../public/')));
+
+app.get('/api/recommended', (req, res) => {
+  Adventures.find({})
+    .then((results) => {
+      res.status(200).send(results);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.get('/api/recommended/:id', (req, res) => {
+  const { id } = req.params;
+  Adventures.find({ _id: id })
+    .then((results) => {
+      res.status(200).send(results);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.put('/api/recommended/:id', (req, res) => {
+  const { id } = req.params;
+  Adventures.findOne({ _id: id })
+    .then((results) => {
+      Adventures.updateOne({ _id: id }, { liked: !results.liked })
+        .then(() => {
+          Adventures.find({ _id: id })
+            .then((data) => {
+              res.status(200).send(data);
+            });
+        })
+        .catch((err) => {
+          res.status(400).send(err);
+        });
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`listening on port ${PORT}`);
 });
