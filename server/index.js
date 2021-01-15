31c7f26b3e47d9d15d7d085a3748881c2e4f9f5c
Saba Teklu <saab.teklu@gmail.com>
2021-01-15 11:56:23 -0700

    complete configuration of jest and circleCI

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
+/* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '../public/'));
+app.use(express.static(path.join(__dirname, '../public/')));
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
